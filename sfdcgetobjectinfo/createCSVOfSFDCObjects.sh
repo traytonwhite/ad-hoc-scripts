81bd784db0a8b2aac67da570d91ab3ef05668009
Trayton White <tbw@traytonwhite.com>
2015-05-08 04:31:18 +0000

    Updating to pull in all fields into a single csv file

--- sfdcgetobjectinfo/createCSVOfSFDCObjects.sh
+++ sfdcgetobjectinfo/createCSVOfSFDCObjects.sh
@@ -3,22 +3,25 @@
 prefix="${1}"
 loginfile="${2}"
 
-objectlist=("Account" "Contact" "Opportunity")
-
-# ping the SFDC instance using the SOAP API to learn what the session ID is for the REST API calls and get the instance
+# ping the SFDC instance using the SOAP API to learn what the session ID is for the REST API calls
 xmlresponse="$(curl -s https://login.salesforce.com/services/Soap/u/33.0 -H "Content-Type: text/xml; charset=UTF-8" -H "SOAPAction: login" -d @"${loginfile}")"
 # session ID is whatever is between the <sessionId> XML tag
 sessionid="$(echo "${xmlresponse}" | sed 's/.*<sessionId>\(.*\)<\/sessionId>.*/\1/')"
-# to determine the SFDC instance, we'll grab the instance of the metadata server
+# to determine the SFDC instance, we'll grab the instance of the server URL
 sfdcinstance="$(echo "${xmlresponse}" | sed 's/.*<serverUrl>https:\/\/\(.*\)\.salesforce\.com.*<\/serverUrl>.*/\1/')"
 
-for object in "${objectlist[@]}"
+objectlisturl="https://${sfdcinstance}.salesforce.com/services/data/v33.0/sobjects/"
+objectlist="${prefix}_sobjectlist.txt"
+curl -s "${objectlisturl}" -H "Authorization: Bearer ${sessionid}" -H "X-PrettyPrint:1" | jq -r '.sobjects[].name' > "${objectlist}"
+
+csvOutput="${prefix}_allobjectsdescribe.csv"
+
+while read object
 do
 	echo "$(date) -- Starting processing of ${object}"
 	jsonResponse="${prefix}_${object}_describe.json"
-	csvOutput="${prefix}_${object}_describe.csv"
 	sfdcurl="https://${sfdcinstance}.salesforce.com/services/data/v33.0/sobjects/${object}/describe/"
 	curl -s "${sfdcurl}" -H "Authorization: Bearer ${sessionid}" -H "X-PrettyPrint:1" > "${jsonResponse}"
-	jq -r '.fields[] | [ .name, .label, .unique, .nillable, .externalId, .type, .relationshipName, ([.referenceTo[]] | join(";")), .cascadeDelete, .calculated, .calculatedFormula, ([.picklistValues[].value] | join(";")) ] | @csv' < "${jsonResponse}" > "${csvOutput}"
+	jq -r '.name as $objectname | .fields[] | [ $objectname, .name, .label, .unique, .nillable, .externalId, .type, .relationshipName, ([.referenceTo[]] | join(";")), .cascadeDelete, .calculated, .calculatedFormula, ([.picklistValues[].value] | join(";")) ] | @csv' < "${jsonResponse}" >> "${csvOutput}"
 	echo "$(date) -- Completed processing of ${object}"
-done
+done < "${objectlist}"
